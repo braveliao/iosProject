71cfb21d5787aa03ed8f7c788bd478eac30aacb8
liaoyong <liao__yong@163.com>
2013-12-03 13:34:18 +0800

    add HelloTestGithub project.

---
+++ HelloWorldTestGitHub/HelloWorldTestGitHub/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloWorldTestGitHub
+//
+//  Created by LiaoYong on 12/3/13.
+//  Copyright (c) 2013 Liao Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HelloWorldTestGitHub/HelloWorldTestGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorldTestGitHub
+//
+//  Created by LiaoYong on 12/3/13.
+//  Copyright (c) 2013 Liao Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
